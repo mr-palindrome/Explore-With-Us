46cfd84e949e6a85d92b9af74bf7af2da96f2ded
Nayan Das <ndas5144@gmail.com>
2023-04-18 23:51:25 +0530

    ADDED VERCEL CONFIGURATIONS

--- Explore_With_Us/settings.py
+++ Explore_With_Us/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -123,8 +123,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+    os.path.join(BASE_DIR, config('STATIC_PATH'))
+] 
 STATIC_ROOT = os.path.join(BASE_DIR, 'assests')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media')
